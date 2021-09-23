0fa66f0637aa1b52de299a079085b41aa2e3c8e7
bertman12 <eddydaniel94@gmail.com>
2021-09-22 20:17:34 -0700

    testing 2

--- dist/registerMessageHandlers.js
+++ dist/registerMessageHandlers.js
@@ -6,19 +6,15 @@ class MessageHandler {
         this.socket = socket;
     }
     observe() {
-        let errorMsg = '';
         try {
-            errorMsg = 'chat-message event error';
             this.socket.on('chat-message', (message) => {
                 this.io.emit('chat-message', message);
                 console.log('check');
             });
-            errorMsg = 'chat-message-edited event error';
             this.socket.on('chat-message-edited', (message) => {
                 console.log('new edited message = ', message.content);
                 this.io.emit('chat-message-edited', message);
             });
-            errorMsg = 'chat-message-deleted event error';
             this.socket.on('chat-message-deleted', (message) => {
                 console.log('deleted message = ', message.content);
                 this.io.emit('chat-message-deleted', message);
--- src/registerMessageHandlers.ts
+++ src/registerMessageHandlers.ts
@@ -4,19 +4,15 @@ export default class MessageHandler{
     constructor(private io, private socket){}
     
     observe(){
-        let errorMsg:string = '';
         try {
-            errorMsg = 'chat-message event error';
             this.socket.on('chat-message', (message: Message)=>{
                 this.io.emit('chat-message', message);
                 console.log('check');
             });
-            errorMsg = 'chat-message-edited event error';
             this.socket.on('chat-message-edited', (message: Message) => {
                 console.log('new edited message = ', message.content);
                 this.io.emit('chat-message-edited', message);
             });
-            errorMsg = 'chat-message-deleted event error';
             this.socket.on('chat-message-deleted', (message: Message) => {
                 console.log('deleted message = ', message.content);
                 this.io.emit('chat-message-deleted', message);
